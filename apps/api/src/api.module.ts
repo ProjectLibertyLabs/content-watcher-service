06ff6978faaf163610cc63671cbe3f6d48a5f189
Wil Wade <wil.wade@amplica.io>
2024-06-28 12:49:31 +0000

    Follow the order of the other services

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -159,7 +159,9 @@ import * as QueueConstants from '@libs/common';
     ScheduleModule.forRoot(),
   ],
   providers: [ApiService],
-  controllers: [HealthController, ScanControllerV1, SearchControllerV1, WebhookControllerV1],
+  // Controller order determines the order of display for docs
+  // v[Desc first][ABC Second], Health, and then Dev only last
+  controllers: [ScanControllerV1, SearchControllerV1, WebhookControllerV1, HealthController],
   exports: [],
 })
 export class ApiModule {}
