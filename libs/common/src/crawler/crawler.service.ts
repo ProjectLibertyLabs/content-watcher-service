d86e2897e635c82996f5bdf9d0f97b2e1225ffd4
Joe Caputo <joseph.caputo@unfinished.com>
2024-07-19 10:08:14 -0400

    fix: process search block list in chunks to avoid OOM

--- libs/common/src/crawler/crawler.service.ts
+++ libs/common/src/crawler/crawler.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectQueue, Processor } from '@nestjs/bullmq';
 import Redis from 'ioredis';
 import { InjectRedis } from '@songkeys/nestjs-redis';
-import { Job, Queue } from 'bullmq';
+import { DelayedError, Job, Queue } from 'bullmq';
 import * as QueueConstants from '../queues/queue-constants';
 import { ChainWatchOptionsDto } from '../dtos/chain.watch.dto';
 import { BaseConsumer } from '../utils/base-consumer';
 import { ContentSearchRequestDto } from '../dtos/content-search-request.dto';
-import { REGISTERED_WEBHOOK_KEY } from '../constants';
 import { ChainEventProcessorService } from '../blockchain/chain-event-processor.service';
 import { BlockchainService } from '../blockchain/blockchain.service';
+
+const CRAWLER_BLOCK_CHUNK_SIZE = 500;
 
 @Injectable()
 @Processor(QueueConstants.REQUEST_QUEUE_NAME, {
@@ -33,21 +34,36 @@ export class CrawlerService extends BaseConsumer {
       let startBlock = job.data.startBlock;
       if (!startBlock) {
         startBlock = await this.blockchainService.getLatestFinalizedBlockNumber();
+        job.data.startBlock = startBlock;
         this.logger.debug(`No starting block specified; starting from end of chain at block ${startBlock}`);
       }
-      const blockList = new Array(job.data.blockCount).fill(0).map((_v, index) => startBlock - index);
+      let blockList = new Array(job.data.blockCount).fill(0).map((_v, index) => startBlock - index);
       blockList.reverse();
-      await this.processBlockList(job.data.clientReferenceId, blockList, job.data.filters);
+
+      // Process block list in chunks so as not to overload the async queue
+      await this.processBlockList(job.data.clientReferenceId, blockList.slice(0, CRAWLER_BLOCK_CHUNK_SIZE), job.data.filters);
+      blockList = blockList.slice(CRAWLER_BLOCK_CHUNK_SIZE);
+
+      if (blockList.length > 0) {
+        job.data.blockCount = blockList.length;
+        await job.updateData(job.data);
+        await job.moveToDelayed(Date.now());
+        throw new DelayedError();
+      }
 
       this.logger.log(`Finished processing job ${job.id}`);
     } catch (error) {
+      if (error instanceof DelayedError) {
+        throw error;
+      }
+
       this.logger.error(`Error processing crawler search job: ${JSON.stringify(error)}`);
       throw error;
     }
   }
 
   private async processBlockList(clientReferenceId: string, blockList: number[], filters: ChainWatchOptionsDto) {
-    this.logger.debug({ minBlock: Math.min(...blockList), maxBlock: Math.max(...blockList) }, 'Processing block list');
+    this.logger.debug(`Processing block list ${Math.min(...blockList)}...${Math.max(...blockList)}`);
     await Promise.all(
       blockList.map(async (blockNumber) => {
         const messages = await this.chainEventService.getMessagesInBlock(blockNumber, filters);
